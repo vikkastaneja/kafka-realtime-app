748b89b1b0b69fec87367ad16fe2704e68904431
vikkastaneja <vikkastaneja@users.noreply.github.com>
2023-06-26 14:47:57 -0700

    Adding producer that ingests streaming data from wikimedia

---
+++ realtime-ingestion/src/test/java/app/innov8r/realtimeingestion/RealtimeIngestionApplicationTests.java
@@ -0,0 +1,13 @@
+package app.innov8r.realtimeingestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeIngestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
