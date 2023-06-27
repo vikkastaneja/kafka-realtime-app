28fd2a5dbba1ae7832c9beaf090cf90c3e8d885c
vikkastaneja <vikkastaneja@users.noreply.github.com>
2023-06-26 17:39:18 -0700

    Kafka consumer without data being stored in DB

---
+++ realtime-ingestion/kafka-consumer-database/src/main/java/app/innov8r/springboot/KafkaDatabaseConsumer.java
@@ -0,0 +1,16 @@
+package app.innov8r.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaDatabaseConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+
+    @KafkaListener (topics = "wikimedia_recentchange", groupId = "myGroup")
+    public void consume(String eventMessage){
+        LOGGER.info(String.format("Event message received: %s", eventMessage));
+    }
+}
